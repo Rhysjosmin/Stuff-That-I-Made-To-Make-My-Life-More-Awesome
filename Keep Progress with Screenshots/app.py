fc0b3bf806229f33d3bf5a0393851be213fcec77
RhysJosmin <55994011+Rhysjosmin@users.noreply.github.com>
2023-04-07 15:34:25 +0530

    Break Clauses

--- Keep Progress with Screenshots/app.py
+++ Keep Progress with Screenshots/app.py
@@ -2,16 +2,24 @@ import schedule
 import pyautogui
 import time
 import datetime
-
+i=0
+MAX_PICS=500
 def Screenshot():
+    global i
+    i=i+1
     myScreenshot = pyautogui.screenshot()
     myScreenshot.save(
         f"./Keep Progress with Screenshots/Screenshots/{str(datetime.datetime.now()).replace('-', '').replace(':', '').replace('.', '').replace(' ', '')+str(time.time_ns())}.png")
 
 
 Screenshot()
-schedule.every(13).minutes.do(Screenshot)
+schedule.every(5).minutes.do(Screenshot)
 
 while True:
+    
     schedule.run_pending()
     time.sleep(10)
+
+    if(i>=MAX_PICS):
+        print(i)
+        break
