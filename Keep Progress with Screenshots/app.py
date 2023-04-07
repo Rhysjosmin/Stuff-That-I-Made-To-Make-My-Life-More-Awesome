a64ad886ac32c068db21a346f317b84b6304a497
RhysJosmin <55994011+Rhysjosmin@users.noreply.github.com>
2023-04-07 15:26:16 +0530

    Cleanup

--- Keep Progress with Screenshots/app.py
+++ Keep Progress with Screenshots/app.py
@@ -1,28 +1,17 @@
-import random
 import schedule
-import urllib.request  
 import pyautogui
 import time
 import datetime
 
-
-
-
-# print(time.time_ns())
-
 def Screenshot():
     myScreenshot = pyautogui.screenshot()
-    myScreenshot.save(f"./Keep Progress with Screenshots/Screenshots/{str(datetime.datetime.now()).replace('-', '').replace(':', '').replace('.', '').replace(' ', '')+str(time.time_ns())}.png")
-# toaster.show_toast("Workout",
-#     "Become Buff",
-#     icon_path=None,
-#     duration=1)
-# webUrl = urllib.request.urlopen(Videos[random.randint(0,len(Videos)-1 )])  
+    myScreenshot.save(
+        f"./Keep Progress with Screenshots/Screenshots/{str(datetime.datetime.now()).replace('-', '').replace(':', '').replace('.', '').replace(' ', '')+str(time.time_ns())}.png")
+
 
 Screenshot()
-schedule.every(1).seconds.do(Screenshot)
+schedule.every(13).minutes.do(Screenshot)
 
 while True:
     schedule.run_pending()
-    time.sleep(1)
-    
+    time.sleep(10)
